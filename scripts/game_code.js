c49127cf65705bf6608dedd689b72e631d58c6bd
JTMBEN <you@example.com>
2025-02-01 16:25:27 +0100

    rajout des phrases

--- scripts/game_code.js
+++ scripts/game_code.js
@@ -1,8 +1,13 @@
 const fruits = ["abricot", "ananas", "avocat", "banane", "cerise", "citron", "clémentine", "datte", "figue", "fraise", "framboise", "grenade", "groseille", "kiwi", "litchi", "mandarine", "mangue", "melon", "mirabelle", "mûre", "myrtille", "nectarine", "noisette", "noix", "orange", "pamplemousse", "papaye", "pastèque", "pêche", "poire", "pomme", "prune", "raisin", "tomate", "abricotier", "amande", "amandier", "ananas", "arbre", "avocatier", "bananier", "basilic", "betterave", "bleuet", "brocoli", "cactus", "cassis", "cerisier", "châtaigne", "châtaignier", "chou", "citronnier", "clémentinier", "cocotier", "courge", "courgette", "cresson", "datte", "dattier", "endive", "estragon", "fenouil", "figuier", "fraisier", "framboisier", "grenadier", "groseillier", "haricot", "kiwi", "laitue", "légume", "lentille", "lierre", "litchi", "mandarinier", "mangue", "manguier", "melon", "menthe", "mûrier", "myrtille", "nectarine", "noisette", "noisetier", "noix", "noyer", "olivier", "orange", "oranger", "pamplemousse", "pamplemoussier", "papaye", "pêcher", "pistache", "pistachier", "poire", "poirier", "pomme", "pommier", "prune", "prunier", "raisin", "raisins", "tomate", "tomates", "romarin", "salade", "sauge", "thym", "topinambour", "trèfle", "tulipe", "valériane", "verveine", "vigne"];
 const beers = ["Heineken", "Stella Artois", "Guinness", "Corona", "Budweiser", "Carlsberg", "Asahi", "Tsingtao", "Peroni", "Beck's", "Foster's", "Kronenbourg 1664", "Leffe", "Hoegaarden", "Tiger", "Singha", "Quilmes", "Brahma", "Paulaner", "Pilsner Urquell", "San Miguel", "Sapporo", "Tuborg", "Kirin", "Baltika", "Victoria Bitter", "Kingfisher", "Chang", "Miller", "Coors", "Blue Moon", "Samuel Adams", "Sierra Nevada", "Modelo", "Pacifico", "Dos Equis", "Tecate", "Amstel", "Grolsch", "Efes", "Mahou", "Moretti", "Super Bock", "Sagres", "Jupiler", "Palm", "Chimay", "Duvel", "Westmalle", "La Chouffe", "Delirium", "Orval", "Kwak", "Brooklyn", "Goose Island", "Lagunitas", "Stone", "Dogfish Head", "Founders", "Bell's", "New Belgium", "Flying Dog", "Rogue", "Deschutes", "Anchor Steam", "Victory", "Left Hand", "Russian River", "Three Floyds", "Mikkeller", "BrewDog", "Weihenstephaner", "Schneider Weisse", "Erdinger", "Warsteiner", "Bitburger", "Spaten", "Löwenbräu", "Augustiner", "Franziskaner", "Hofbräu", "Ayinger", "Schlenkerla", "Krombacher", "DAB", "Veltins", "Köstritzer", "Maisel's", "König Ludwig", "Alhambra", "Estrella Damm", "Cruzcampo", "San Miguel España", "Żywiec", "Tyskie", "Lech", "Okocim", "Perła", "Heineken Brasil", "Antarctica", "Skol", "Kaiser", "Bohemia", "Devassa", "Xingu", "Colorado", "Eisenbahn", "Baden Baden", "Dado Bier", "Way Beer", "Bodebrown", "Seasons", "Invicta", "DUM", "2 Cabeças", "Wäls", "Tupiniquim", "Urbana", "Backer", "Bamberg", "Dama Bier", "Morada", "Schornstein", "Stannis", "Brewpoint", "HopMundi", "Kraüt", "Dogma", "Outro Mundo", "Vinil", "3 Lobos", "Cerveja Blondine", "Cervejaria Nacional", "Cervejaria Campanária", "Cervejaria Bodoque", "Cervejaria Providência"];
 
-const words_index = [beers, fruits];
+const phrasefruit = ["Smoothie fraise-banane délicieux", "Achète des pommes et poires", "Savoureuse salade de fruits frais", "Oranges riches en vitamine C", "Délicieuse tarte aux cerises", "Grignoter raisins en télé", "Pique-nique avec pastèque fraîche", "Mangues juteuses et sucrées", "Avocatier planté dans jardin", "Figues, fromage et miel délicieux", "Confiture fraise maison", "Pêche juteuse d'été", "Gâteau citron meringué", "Ananas tranché frais", "Sorbet framboise rafraîchissant", "Jus de pomme bio", "Parfait kiwi et yaourt", "Coulis de fruits rouges", "Petit déjeuner banane", "Salade mangue avocat"];
+const phrasebiere = ["Dégustation de bière locale", "Bière artisanale houblonnée", "Bière blonde rafraîchissante", "Bière brune maltée", "Bière IPA amère et fruitée", "Bière ambrée caramelisée", "Bière stout chocolatée", "Bière de saison épicée", "Bière lager légère", "Bière sans alcool", "Bière au gingembre piquante", "Bière fruitée à la framboise", "Bière blanche citronnée", "Bière de Noël épicée", "Bière au miel douce", "Bière au café intense", "Bière à la cerise", "Bière à la mangue", "Bière à la pêche", "Bière au citron"];
+
+words_index = [[beers, fruits], [phrasebiere, phrasefruit]];
 const wordchoise = document.getElementById("word-list");
+
+const typechoise = document.getElementById("word-phrase");
 
 const displayArea = document.getElementById('display-area');
 const inputArea = document.getElementById('input-area');
@@ -44,10 +49,9 @@ function checkWord() {
 }
 let actualWordlist = [];
 
-function wordListConstructor (){
-    actualWordlist = [...words_index[parseInt(wordchoise.value)]]; // récupére la liste saisie par l'utilisateur et remplace toutes les valeurs de actualWordlist
-    //console.log(words_index[wordchoise.value])
-    //console.log(actualWordlist);
+function wordListConstructor() {
+    actualWordlist = [...words_index[parseInt(typechoise.value)][parseInt(wordchoise.value)]];
+    console.log(actualWordlist);  // Pour vérifier si la liste est correcte
 }
 
 function game_points_count(){
@@ -164,7 +168,7 @@ reset.addEventListener("click",raz);
 
 
 wordchoise.addEventListener('change',wordListConstructor);
-
+typechoise.addEventListener('change',wordListConstructor);
 
 
 // () => {
